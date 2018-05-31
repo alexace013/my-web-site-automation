35c93fc697960cf1b1d6da28e491c3921ceed5e7
alexace013 <alexace013@gmail.com>
2018-05-31 19:51:06 +0300

    fixed pom.xml file. Modified OSControllerTest and PropertyControllerTest. Added txt file -> need create README.md and delete this txt file

--- src/test/java/tests/unit/utils/OSControllerTest.java
+++ src/test/java/tests/unit/utils/OSControllerTest.java
@@ -1,18 +1,42 @@
 package tests.unit.utils;
 
+import lombok.extern.log4j.Log4j;
 import org.junit.Assert;
 import org.junit.Test;
 import utils.OSController;
 
+import java.io.IOException;
+
+@Log4j
 public class OSControllerTest {
+
+    private static final String WINDOWS_TEXT = "windows";
+    private static final String UNIX_TEXT = "unix";
+    private static final String SOLARIS_TEXT = "solaris";
+    private static final String MAC_TEXT = "mac";
+    private static final String ASSERT_MESSAGE = "OS isn't %s";
 
     @Test
     public void checkOS() {
-        // TODO need add logic
-        Assert.assertTrue("select correct method or assert", OSController.isUnix());
-        Assert.assertFalse("select correct method", OSController.isWindows());
-        Assert.assertFalse("select correct method", OSController.isSolaris());
-        Assert.assertFalse("select correct method", OSController.isMac());
+        if (OSController.isWindows()) {
+            log.info(WINDOWS_TEXT);
+            Assert.assertTrue(String.format(ASSERT_MESSAGE, WINDOWS_TEXT.toUpperCase()), !OSController.isWindows());
+        } else if (OSController.isUnix()) {
+            log.info(UNIX_TEXT);
+            Assert.assertTrue(String.format(ASSERT_MESSAGE, UNIX_TEXT.toUpperCase()), !OSController.isUnix());
+        } else if (OSController.isSolaris()) {
+            log.info(SOLARIS_TEXT);
+            Assert.assertTrue(String.format(ASSERT_MESSAGE, SOLARIS_TEXT.toUpperCase()), !OSController.isSolaris());
+        } else if (OSController.isMac()) {
+            log.info(MAC_TEXT);
+            Assert.assertTrue(String.format(ASSERT_MESSAGE, MAC_TEXT.toUpperCase()), !OSController.isMac());
+        } else {
+            try {
+                throw new IOException();
+            } catch (IOException e) {
+                log.error("This isn't any OS", e);
+            }
+        }
     }
 
 }
