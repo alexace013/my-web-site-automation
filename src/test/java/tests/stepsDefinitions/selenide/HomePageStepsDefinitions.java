610809acbcd20397d32c85ff0f75280573160b57
alexace013 <alexace013@gmail.com>
2019-06-02 02:18:10 +0300

    implemented step definitions related with input fields on home page on Selenide side. * need refactoring.

--- src/test/java/tests/stepsDefinitions/selenide/HomePageStepsDefinitions.java
+++ src/test/java/tests/stepsDefinitions/selenide/HomePageStepsDefinitions.java
@@ -1,21 +1,80 @@
 package tests.stepsDefinitions.selenide;
 
+import static com.codeborne.selenide.Condition.visible;
+import static java.lang.String.format;
+import static org.openqa.selenium.By.xpath;
+
 import com.alexanderbakhin.site.IHomePage;
-import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
 import controller.PropertyController;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.openqa.selenium.By;
+import org.assertj.core.api.Assertions;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import tests.fixtures.SeleniumFixture;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class HomePageStepsDefinitions extends SeleniumFixture {
 
+    private static final String WAIT_30_SEC = PropertyController.loadProperty("wait.timeout.30sec");
     private static final String COPYRIGHT_SYMBOL = PropertyController.loadProperty("home.page.copyright.symbol");
     private static final String COPYRIGHT_TEXT = PropertyController.loadProperty("home.page.copyright.text");
 
     @When("^user clicks on DOWNLOAD RESUME button on Selenide$")
-    public void userClicksOnDownloadResumeButton() {
-        $(By.xpath(IHomePage.DOWNLOAD_RESUME_BUTTON)).shouldBe(Condition.visible).click();
+    public void userClicksOnDownloadResumeButtonOnSelenide() {
+        $(xpath(IHomePage.DOWNLOAD_RESUME_BUTTON)).shouldBe(visible).click();
+    }
+
+    @When("^user inputs \"([^\"]*)\" data in name field on Selenide$")
+    public void userInputsDataInNameFieldOnSelenide(final String data) {
+        inputDataIntoField(IHomePage.MessagePanel.NAME, data);
+    }
+
+    @When("^user inputs \"([^\"]*)\" data in e-mail field on Selenide$")
+    public void userInputsDataInEmailFieldOnSelenide(final String data) {
+        inputDataIntoField(IHomePage.MessagePanel.E_MAIL, data);
+    }
+
+    @When("^user inputs \"([^\"]*)\" data in country field on Selenide$")
+    public void userInputsDataInCountryFieldOnSelenide(final String data) {
+        inputDataIntoField(IHomePage.MessagePanel.COUNTRY, data);
+    }
+
+    @When("^user inputs \"([^\"]*)\" data in city field on Selenide$")
+    public void userInputsDataInCityFieldOnSelenide(final String data) {
+        inputDataIntoField(IHomePage.MessagePanel.CITY, data);
+    }
+
+    @When("^user inputs \"([^\"]*)\" data in phone field on Selenide$")
+    public void userInputsDataInPhoneFieldOnSelenide(final String data) {
+        inputDataIntoField(IHomePage.MessagePanel.PHONE, data);
+    }
+
+    private static void inputDataIntoField(final IHomePage.MessagePanel messagePanel, final String data) {
+        $(xpath(format(IHomePage.INPUT_FIELDS, messagePanel.getField()))).shouldBe(visible).setValue(data);
+    }
+
+    @When("^user inputs \"([^\"]*)\" data in message textarea on Selenide$")
+    @And("^user inputs \"([^\"]*)\" data in message field on Selenide$")
+    public void userInputsDataInMessageFieldOnSelenide(final String data) {
+        $(xpath(IHomePage.MESSAGE_TEXT_AREA)).shouldBe(visible).setValue(data);
+    }
+
+    @When("^user clicks on SEND button on Selenide$")
+    public void userClicksOnSendButtonOnSelenide() {
+        $(xpath(IHomePage.SEND_BUTTON)).shouldBe(visible).click();
+    }
+
+    @Then("^form was sent successfully with alert \"([^\"]*)\" text on Selenide$")
+    public void formWasSentSuccessfullyWithTextOnSelenide(final String text) {
+        WebDriverWait driverWait = new WebDriverWait(WebDriverRunner.getWebDriver(), Integer.parseInt(WAIT_30_SEC));
+        driverWait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = WebDriverRunner.getWebDriver().switchTo().alert();
+        alert.accept();
+        Assertions.assertThat(alert.getText()).as("alert message is not correct.").isEqualTo(text);
     }
 }
--- src/test/resources/features/HomePage_selenide.feature
+++ src/test/resources/features/HomePage_selenide.feature
@@ -15,3 +15,18 @@ Feature: Home Page tests on Selenide
     Examples:
       | google drive resume url                                                |
       | https://drive.google.com/file/d/1SZAIO2978XL0arryh19Wumsag7W0eD1_/view |
+
+  Scenario Outline: 0003 - send test and generate message from home page on Selenide
+    When user inputs "<name>" data in name field on Selenide
+    And user inputs "<email>" data in e-mail field on Selenide
+    And user inputs "<country>" data in country field on Selenide
+    And user inputs "<city>" data in city field on Selenide
+    And user inputs "<phone>" data in phone field on Selenide
+    And user inputs "<message>" data in message textarea on Selenide
+    And user clicks on SEND button on Selenide
+#    Then form was sent successfully with alert "Message was sent. Thanks!" text on Selenide
+
+    Examples:
+      | name     | email         | country     | city     | phone             | message           |
+      | TestName | test@mail.com | TestCountry | TestCity | +12(345)678-90-12 | Test Message Text |
+#      | generate | generate      | generate    | generate | generate          | generate          |
